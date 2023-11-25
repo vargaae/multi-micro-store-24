040b6ba98c3ac63974807c3f877964e859a1f1c8
vargaae <vargaae@hotmail.com>
2023-11-25 22:49:25 +0100

    05003-initalizing routes

--- src/App.js
+++ src/App.js
@@ -1,46 +1,22 @@
-import { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
 
 import "./App.scss";
+import Home from "./routes/home/home.component";
 import Footer from "./components/footer/footer.component";
-import SearchBox from "./components/search-box/search-box.component";
-import CategoryDirectory from "./components/category-dir/category-dir.component";
 
-import categories from "./data";
+const Shop = () => {
+  return <h1>SHOP component</h1>;
+};
 
 const App = () => {
-  const [searchField, setSearchField] = useState("");
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    setProducts(categories);
-  }, []);
-
-  const onSearchChange = (event) => {
-    const searchFieldString = event.target.value.toLocaleLowerCase();
-    setSearchField(searchFieldString);
-  };
-
-  const filteredProducts = products.filter((user) => {
-    return user.title.toLocaleLowerCase().includes(searchField);
-  });
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img
-          src="https://pilis-solar.hu/files/menu/46/webshop.png"
-          className="App-logo"
-          alt="logo"
-        />
-      </header>
-      <SearchBox
-        onChangeHandler={onSearchChange}
-        placeholder="search products"
-        className="products-search-box"
-      />
-      <CategoryDirectory products={filteredProducts} />
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+      </Routes>
       <Footer />
-    </div>
+    </>
   );
 };
 
---
+++ src/routes/home/home.component.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import "./../../App.scss";
+import SearchBox from "./../../components/search-box/search-box.component";
+import CategoryDirectory from "./../../components/category-dir/category-dir.component";
+
+import categories from "./../../data";
+
+const Home = () => {
+  const [searchField, setSearchField] = useState("");
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    setProducts(categories);
+  }, []);
+
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
+  };
+
+  const filteredProducts = products.filter((user) => {
+    return user.title.toLocaleLowerCase().includes(searchField);
+  });
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img
+          src="https://pilis-solar.hu/files/menu/46/webshop.png"
+          className="App-logo"
+          alt="logo"
+        />
+      </header>
+      <SearchBox
+        onChangeHandler={onSearchChange}
+        placeholder="search products"
+        className="products-search-box"
+      />
+      <CategoryDirectory products={filteredProducts} />
+    </div>
+  );
+};
+
+export default Home;
