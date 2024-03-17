35bd3abd7f2b9f8b58f4e92f42e95154987c2f4e
vargaae <vargaae@hotmail.com>
2024-03-17 17:09:18 +0100

    1060010 SetUp Sign Up Form

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -4,6 +4,7 @@ import {
   signInWithGooglePopup,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 
 const SingIn = () => {
   const logGoogleUser = async () => {
@@ -16,6 +17,7 @@ const SingIn = () => {
     <div className="sign">
       <h1>- Sign In Page -</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+      <SignUpForm />
     </div>
   );
 };
