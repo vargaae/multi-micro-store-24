eea049a23a86d68182ad3a89efcdf6aab48bb1c2
vargaae <vargaae@hotmail.com>
2024-04-19 01:08:22 +0200

    110007-Create Category Preview Component

---
+++ src/components/category-preview/CategoryPreview.jsx
@@ -0,0 +1,24 @@
+import ProductCard from "../../components/product-card/ProductCard";
+
+import "./category-preview.styles.scss";
+
+const CategoryPreview = ({ title, products }) => {
+  return (
+    <div className="category-preview-container">
+      <h2>
+        <span className="title">{title.toUpperCase()}</span>
+      </h2>
+      <div className="preview">
+        {products
+          // eslint-disable-next-line react/prop-types
+          .slice()
+          .filter((_, idx) => idx < 4)
+          .map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default CategoryPreview;
--- src/routes/shop/Shop.jsx
+++ src/routes/shop/Shop.jsx
@@ -1,6 +1,6 @@
 import { Fragment, useContext } from "react";
 
-import ProductCard from "../../components/product-card/ProductCard";
+import CategoryPreview from "../../components/category-preview/CategoryPreview";
 
 import { CategoriesContext } from "../../contexts/Categories.context";
 
@@ -10,18 +10,14 @@ const Shop = () => {
   const { categoriesMap } = useContext(CategoriesContext);
 
   return (
-    <>
-      {Object.keys(categoriesMap).map((title) => (
-        <Fragment key={title}>
-          <h2>{title}</h2>
-          <div className="App products-container">
-            {categoriesMap[title].map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
-          </div>
-        </Fragment>
-      ))}
-    </>
+    <div className="shop-container">
+      {Object.keys(categoriesMap).map((title) => {
+        const products = categoriesMap[title];
+        return (
+          <CategoryPreview key={title} title={title} products={products} />
+        );
+      })}
+    </div>
   );
 };
 
