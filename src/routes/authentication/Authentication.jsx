856f4e0abe6baadac2bac7db57137a2efca3c9fd
vargaae <vargaae@hotmail.com>
2024-05-05 13:20:38 +0200

    111007-4-CleanUp Styled-Component Authentication

--- src/routes/authentication/Authentication.jsx
+++ src/routes/authentication/Authentication.jsx
@@ -1,14 +1,14 @@
-import "./authentication.styles.scss";
+import { AuthenticationContainer } from "./Authentication.styles";
 
 import SignInForm from "../../components/sign-in-form/SignInForm";
 import SignUpForm from "../../components/sign-up-form/SignUpForm";
 
 const Authentication = () => {
   return (
-    <div className="authentication-container">
+    <AuthenticationContainer>
       <SignInForm />
       <SignUpForm />
-    </div>
+    </AuthenticationContainer>
   );
 };
 
--- src/routes/authentication/Authentication.styles.jsx
+++ src/routes/authentication/Authentication.styles.jsx
@@ -1,8 +1,24 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const AuthenticationContainer = styled.div`
-  display: flex;
-  max-width: 900px;
+  /* Authentication page: 
+mobile-first: On screens that are 600px wide or more, there is one column in a row */
+  margin: 15px;
   justify-content: space-between;
-  margin: 30px auto;
+
+  /*  On screens that are 769px wide or more, there is one column in a row,
+On screens that are 769px wide or more, make the columns in the middle */
+  @media screen and (min-width: 769px) {
+    display: flex;
+    width: 769px;
+    margin: 20px auto;
+  }
+
+  /*  On screens that are 900px wide or more, go from one column to two columns, 
+so make the columns stack on top of each other instead of next to each other */
+  @media screen and (min-width: 900px) {
+    display: flex;
+    width: 900px;
+    margin: 30px auto;
+  }
 `;
