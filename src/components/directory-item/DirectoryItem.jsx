5bb9af3de07eac1ff1969b970264f138335e0d69
vargaae <vargaae@hotmail.com>
2024-05-05 21:31:20 +0200

    111007-6-fixed Styled-Component props('imgurl')error with $ prefix

--- src/components/directory-item/DirectoryItem.jsx
+++ src/components/directory-item/DirectoryItem.jsx
@@ -7,7 +7,7 @@ import {
 const DirectoryItem = ({ product: { title, imageUrl, ShopNow } }) => {
   return (
     <DirectoryItemContainer to={`shop/${title}`}>
-      <BackgroundImage imgurl={imageUrl} />
+      <BackgroundImage $imgurl={imageUrl} />
       <Body>
         <h2>{title}</h2>
         <p className={ShopNow}>{ShopNow}</p>
--- src/components/directory-item/DirectoryItem.styles.jsx
+++ src/components/directory-item/DirectoryItem.styles.jsx
@@ -7,7 +7,7 @@ export const BackgroundImage = styled.div`
   height: 100%;
   background-size: cover;
   background-position: center;
-  background-image: ${({ imgurl }) => `url(${imgurl})`};
+  background-image: ${({ $imgurl }) => `url(${$imgurl})`};
 `;
 
 export const Body = styled.div`
