e07a540d161f2a0113fd33a5b77872067b35bafa
vargaae <vargaae@hotmail.com>
2024-06-17 23:31:48 +0200

    302-07-02-SetUp Redux-persist successfully

--- src/Main.jsx
+++ src/Main.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import { Provider } from "react-redux";
-import { store } from "./store/store";
+import { store, persistor } from "./store/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 import App from "./App";
 
@@ -12,7 +13,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
+
 import { setupListeners } from "@reduxjs/toolkit/query";
 
 import logger from "redux-logger";
@@ -11,15 +24,29 @@ const middleWares = [
   import.meta.env.NODE_ENV === "development" && logger,
 ].filter(Boolean);
 
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
   [strApi.reducerPath]: strApi.reducer,
-  reducer: rootReducer,
+  reducer: persistedReducer,
   //   middleware: (getDefaultMiddleware) =>
   //     getDefaultMiddleware().concat(middleWares),
   // });
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(strApi.middleware, middleWares),
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(strApi.middleware, middleWares),
 });
+
+export let persistor = persistStore(store);
 // optional, but required for refetchOnFocus/refetchOnReconnect behaviors
 // see `setupListeners` docs - takes an optional callback as the 2nd arg for customization
 setupListeners(store.dispatch);
